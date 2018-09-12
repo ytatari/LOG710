8e149e5d19f0f4c0962f7f0abb6b9b740b32c9fb
tcaspar <tiahiti.caspar.1@ens.etsmtl.ca>
2018-09-12 11:29:08 -0400

    first commit

--- Lab_01/RunCmd.c
+++ Lab_01/RunCmd.c
@@ -1,1 +1,24 @@
-toto
+#include <stdio.h>
+#include <stdlib.h>
+
+int main (int argc, char* argv[]) {
+	int pid = fork();
+
+	if (pid<0){
+		printf("Erreur de fork.\n");
+		printf("toto %c \n", argv[1]);
+		return 1;
+	}
+	else if (pid) {
+		printf("je suis le parent, pid=%d \n");
+		printf("le id de mon fils=%d \n", pid);
+		printf("toto %d \n", argc);
+	}
+	else {
+		printf("je suis le fils, mon id= %d \n");
+		printf("l'argument est %s \n", argv[3]);
+		return 1;
+	}
+	return 0;
+
+}
